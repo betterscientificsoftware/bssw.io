7762de026df00c696118956fdec55b53121780e3
Roscoe A. Bartlett <rabartl@sandia.gov>
2021-03-02 11:24:03 -0500

    Explicitly check for saved input file (#363, #702)
    These are not pretty complete system-level tests of the basic behavior of
    wikize_refs.py but much more testing is needed to pin down allowed and not
    allowed input file contents.  And unit testing should be added as well.

--- utils/tests/run_wikize_ref_diff_test.cmake
+++ utils/tests/run_wikize_ref_diff_test.cmake
@@ -6,9 +6,10 @@
 #  cmake \
 #    -D TEST_NAME=<testName> \
 #    -D INPUT_FILE=<inputFile> \
-#    -D OUTPUT_FILE=<outputFile> \
 #    -D WIKIZE_REFS_ARGS=<wikizeRefsArgs> \
-#    -D EXPECTED_OUTPUT_FILE=<expectedOutputFile> \
+#    -D COMPARE_SAVED_INPUT_FILE=[TRUE|FALSE] \
+#    [-D OUTPUT_FILE=<outputFile>] \
+#    [-D EXPECTED_OUTPUT_FILE=<expectedOutputFile>] \
 #    -P run_wikize_ref_diff_test.cmake
 #
 # Here, the filepaths for <inputFile> and <expectedOutputFile> should be
@@ -24,6 +25,11 @@
 #
 #   diff <expectedOutputFile> <outputFile>
 #
+# If COMPARE_SAVED_INPUT_FILE=TRUE is passed in, then the file
+# name(<inputFile>)~ will be compared to <inputFile> make make sure the
+# orginal file is saved.  If COMPARE_SAVED_INPUT_FILE=FALSE, then that file
+# can not exist after running wikize_refs.py.
+#
 # If the wikize_refs.py command runs to completion without error and the diff
 # of the output file against the expected output file returns no change, then
 # this CMake -P script will return 0.  Otherwise, it will return non-zero and
@@ -32,6 +38,8 @@
 
 #include(CMakePrintHelpers)
 #cmake_print_variables(TEST_NAME INPUT_FILE OUTPUT_FILE WIKIZE_REFS_ARGS EXPECTED_OUTPUT_FILE)
+
+get_filename_component(inputFileName "${INPUT_FILE}" NAME)
 
 # A) Set up test directory to hold the input and output files
 
@@ -50,7 +58,6 @@ file(COPY "${INPUT_FILE}" DESTINATION "${TEST_NAME}")
 # C) Run the wikize_refs command
 
 set(wikizeRefs "${CMAKE_CURRENT_LIST_DIR}/../wikize_refs.py")
-get_filename_component(inputFileName "${INPUT_FILE}" NAME)
 set(wikizeRefsCmnd ${wikizeRefs} ${WIKIZE_REFS_ARGS} ${inputFileName})
 
 message("Running: ${wikizeRefsCmnd}")
@@ -67,7 +74,6 @@ endif()
 
 # D) Compare the output file to expected output
 
-
 if (NOT EXPECTED_OUTPUT_FILE STREQUAL "")
   message("Diffing expected output '${EXPECTED_OUTPUT_FILE}' to output '${OUTPUT_FILE}'")
   execute_process(
@@ -80,4 +86,25 @@ if (NOT EXPECTED_OUTPUT_FILE STREQUAL "")
   endif()
 else()
   message("Skipping diff of output file since none given!")
+endif()
+
+# E) Compared the saved input file
+
+set(savedInputFileName "${inputFileName}~")
+
+if (COMPARE_SAVED_INPUT_FILE)
+  message("Diffing saved input file '${savedInputFileName}' to input '${INPUT_FILE}'")
+  execute_process(
+    COMMAND diff "${savedInputFileName}" "${INPUT_FILE}"
+    WORKING_DIRECTORY "${TEST_NAME}"
+    RESULT_VARIABLE diffRtnCode
+    )
+  if (NOT diffRtnCode EQUAL 0)
+    message(FATAL_ERROR "diff returned '${diffRtnCode}'")
+  endif()
+else()
+  message("Ensure that saved input file '${savedInputFileName}' does not exist!")
+  if (EXISTS "${savedInputFileName}")
+    message(FATAL_ERROR "The saved input file '${savedInputFileName}' exists when it should not!")
+  endif()
 endif()
