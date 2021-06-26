b02d502e800c35fc3947776bb0c4f53b623319b6
Roscoe A. Bartlett <rabartl@sandia.gov>
2021-06-26 18:26:40 -0400

    Add ENABLE_LINK_CHECK_TESTS and LINK_CHECK_TEST
    This disabled tests that do network communication by default.

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -2,6 +2,21 @@
 #
 # Simple driver to run tests in one command
 
+enable_link_check_tests=0
+
+while (( "$#" )); do
+  case "$1" in
+    -l|--enable-link-check-tests)
+      enable_link_check_tests=1
+      shift
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+  esac
+done
+
 if [[ ! -d build_tests ]] ; then
     mkdir build_tests
 fi
@@ -12,6 +27,12 @@ if [[ -e CMakeCache.txt ]] || [[ -d CMakeFiles/ ]] ; then
   rm -r CMake*
 fi
 
-cmake ..
+if [[ "${enable_link_check_tests}" == "1" ]] ; then
+  cmake_args="-DENABLE_LINK_CHECK_TESTS:BOOL=ON"
+else
+  cmake_args=
+fi
+
+cmake ${cmake_args} ..
 
 ctest
