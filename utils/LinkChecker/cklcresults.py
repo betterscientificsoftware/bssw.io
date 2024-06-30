f53a1e0721b406a174d0462b6a655a65460ced36
Mark C. Miller <miller86@llnl.gov>
2024-06-30 11:24:27 -0700

    add presistent lists

--- utils/LinkChecker/cklcresults.py
+++ utils/LinkChecker/cklcresults.py
@@ -1,16 +1,4 @@
-f = open("linkchecker-all.out")
-lines = f.readlines()
-f.close()
-
-
-#
-# Parse lines for status records which like like...
-#
-# URL        `file:///home/runner/work/bssw.io/bssw.io/Articles/Blog/2020-01-usrse.md'
-# .
-# .
-# .
-# Result     Valid
+import ast, datetime
 
 def AddEntryToRecord(cr, key, line):
     assert key not in cr.keys(), f"duplicate key {key}, {line}, {cr}"
@@ -21,18 +9,32 @@ def StartNewRecord(cr, key, line):
     line = line[1:-2]
     AddEntryToRecord(cr, key, line)
 
-def AddRecord(records, cr, key, line):
+def AddRecord(records, cr, key, line, now):
     AddEntryToRecord(cr, key, line)
+    AddEntryToRecord(cr, 'Date', now)
     records.append(cr.copy())
     cr.clear()
 
 def AppendLineToPreviousKey(cr, key, line):
     cr[key] += line
 
+# Open log and read all lines into a python list object
+f = open('linkchecker-all.out','r')
+lines = f.readlines()
+f.close()
+
+#
+# Process contents of log, looking for groups of lines defining
+# each link tested and its results. Each link is its own dict
+# object (record) and we append each record to a list, records.
+#
 records = []
 currentRecord = {}
 inRecord = False
 prevKey = ''
+print("Consider date resolution for PRs\n")
+nowdate = datetime.datetime.now()
+nowstr = ' '*11 + nowdate.strftime("%Y-%m-%d")
 for line in lines:
 
     key = line[:11].replace(' ','')
@@ -46,28 +48,85 @@ for line in lines:
     elif key == "Result":
         assert inRecord, "Problem ending new record"
         inRecord = False
-        AddRecord(records, currentRecord, key, line)
+        AddRecord(records, currentRecord, key, line, nowstr)
     elif inRecord:
         AddEntryToRecord(currentRecord, key, line)
-    #else:
-    #    print(f'Skipping "{line}"')
 
     if key != '':
         prevKey = key
 
+#
+# Open trouble_links.txt and read all contents
+#
+trouble_links = []
+with open('../../utils/LinkChecker/trouble_links.txt','r') as file:
+    for line in file:
+        trouble_links.append(ast.literal_eval(line.strip()))
+trouble_links_original_size = len(trouble_links)
+
+#
+# Open bad_links.log 
+#
+bad_links = []
+with open('../../utils/LinkChecker/bad_links.txt','r') as file:
+    for line in file:
+        bad_links.append(ast.literal_eval(line.strip()))
+bad_links_original_size = len(bad_links)
+
+#
+# Process all the links, looking for trouble links.
+# Links that have consistently been troubled are deemed bad.
+#
 for r in records:
 
     # ignore the entry for the .md file itself
     if 'ParentURL' not in r.keys():
         continue
 
-    goodURL = True
+    linkOK = True
     if '200' not in r['Result'][:20]:
         if r['Result'] == 'Valid':
             if 'Warning' in r.keys() and 'Redirected' not in r['Warning']:
-                goodURL = False
+                linkOK = False
         else:
-            goodURL = False
+            linkOK = False
 
-    if not goodURL:
-        print(f"{r['URL']} in \n\t {r['ParentURL'][41:]}")
+    prev_trouble_records = [x for x in trouble_links if x['URL'] == r['URL']]
+
+    if linkOK:
+
+        #
+        # If link is good, make sure any instance of it being in trouble in
+        # the past is removed.
+        #
+        for x in prev_trouble_records:
+            try:
+                trouble_links.remove(x)
+            except:
+                pass
+
+    else:
+
+        if prev_trouble_records == []:
+            trouble_links.append(r)
+        else:
+            for x in prev_trouble_records:
+                date = datetime.datetime.strptime(x['Date'],'%Y-%m-%d')
+                if (nowdate - date).days > 90:
+                    bad_links += [x] 
+
+#
+# Update trouble_links file if modified
+#
+if len(trouble_links) != trouble_links_original_size:
+    with open('../../utils/LinkChecker/trouble_links.txt','w') as file:
+        for rec in trouble_links:
+            file.write(str(rec)+'\n')
+
+#
+# Update bad_links file if modified
+#
+if len(bad_links) > bad_links_original_size:
+    with open('../../utils/LinkChecker/bad_links.txt','w') as file:
+        for rec in bad_links:
+            file.write(str(rec)+'\n')
